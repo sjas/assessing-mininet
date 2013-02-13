ad2de9676ff4adfe11173d8497f60c1f233fb0ca
sjas <teilzeittresor@web.de>
2013-02-13 13:36:03 +0000

    added current startup scripts

---
+++ run_dfn-topology-test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# use with floodlight running on host
+
+sudo mn --controller remote,ip=10.0.2.2 --custom /home/mininet/ktr/parser/raw-material/Dfn.graphml-generated-Mininet-Topo.py --topo generated --test iperf
+
---
+++ run_dfn_local.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# use with floodlight running on host
+
+sudo mn --custom /home/mininet/ktr/parser/raw-material/Dfn.graphml-generated-Mininet-Topo.py --topo generated --test iperf
+
