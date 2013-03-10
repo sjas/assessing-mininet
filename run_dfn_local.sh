b30899879828de2451bac98b2bd23b3adb2f61bf
sjas <teilzeittresor@web.de>
2013-03-10 13:29:22 +0100

    fixed runscript local (added tc as link)

--- run_dfn_local.sh
+++ run_dfn_local.sh
@@ -2,5 +2,5 @@
 
 # use with floodlight running on host
 
-sudo mn --custom /home/mininet/ktr/parser/raw-material/Dfn.graphml-generated-Mininet-Topo.py --topo generated --test iperf
+sudo mn --link tc --custom /home/mininet/ktr/parser/raw-material/Dfn.graphml-generated-Mininet-Topo.py --topo generated --test iperf
 
