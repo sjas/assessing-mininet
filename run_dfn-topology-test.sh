23da10840edc451b1f1ed284100d0a8c43c09a1a
sjas <teilzeittresor@web.de>
2013-03-10 06:39:15 +0100

    fixed mn CLI call to work with bw statements by specifying TC as link

--- run_dfn-topology-test.sh
+++ run_dfn-topology-test.sh
@@ -2,5 +2,5 @@
 
 # use with floodlight running on host
 
-sudo mn --controller remote,ip=10.0.2.2 --custom /home/mininet/ktr/parser/raw-material/Dfn.graphml-generated-Mininet-Topo.py --topo generated --test iperf
+sudo mn --controller remote,ip=10.0.2.2 --link tc --custom /home/mininet/ktr/parser/raw-material/Dfn.graphml-generated-Mininet-Topo.py --topo generated --test iperf
 
