bbe7bb2187b1a17440a6dba2d8b03e32b253e3e7
sjas <teilzeittresor@web.de>
2013-03-10 13:31:00 +0100

    deleted, nothing works without floodlights learning capabilities, so using a local controller is absolutely no use

--- run_dfn_local.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# use with floodlight running on host
-
-sudo mn --link tc --custom /home/mininet/ktr/parser/raw-material/Dfn.graphml-generated-Mininet-Topo.py --topo generated --test iperf
-
