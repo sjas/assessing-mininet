8995a2ae5935d455c2c1f5b281f06028b657ee19
sjas <teilzeittresor@web.de>
2013-03-27 05:41:34 -0700

    added itg install script

---
+++ install-DITG.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# FOR THIS TO WORK, PLACE PATCH FILES IN ~/D-ITG-PATCH
+#
+# create working dir
+mkdir D-ITG
+cd D-ITG
+# get itg source for linux
+wget http://traffic.comics.unina.it/software/ITG/codice/D-ITG-2.8.0-rc1.tgz
+# untar
+tar xf D-ITG-2.8.0-rc1.tgz
+# cd
+cd D-ITG-2.8.0-rc1
+cp ~/D-ITG-PATCH/* .
+# next line won't work since patch cannot be aplied anymore!
+# patch was for another version and is out of sync now.
+#patch -p1 --dry-run < ITGDec.patch
+#cd src
+#make
+echo
+echo 'apply patch by hand now!'
+echo 'it is around line 545, not 535 or so.'
+echo 'then cd src/ and make...'
