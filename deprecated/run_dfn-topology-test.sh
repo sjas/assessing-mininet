846bf9e01e7b733eae8cfbd869b57490b70b12fa
sjas <teilzeittresor@web.de>
2013-03-30 18:08:13 +0100

    removed deprecated folder

--- deprecated/run_dfn-topology-test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# use with floodlight running on host
-
-sudo mn --controller remote,ip=10.0.2.2 --link tc --custom /home/mininet/ktr/parser/raw-material/Dfn.graphml-generated-Mininet-Topo.py --topo generated --test iperf
-
