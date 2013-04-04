75b17824c00e02ba2ea004f1fe6b898d1ffebc10
sjas <teilzeittresor@web.de>
2013-04-03 17:32:43 -0700

    tempsave testsuite

--- ITGscripts/full-test-suite.sh
+++ ITGscripts/full-test-suite.sh
@@ -4,8 +4,8 @@
 #
 # NEEDS: sudo apt-get install sshpass -y
 #
-# ssh's automatically into each machine, starting everything
-# <CTRL-D> a lot after the flow is sent completely to get out of all shells
+# ssh's automatically into each machine, starting everything.
+# how or if this terminates, only god will ever know.
 #
 ###########################################################################
 #currently unneeded:
@@ -15,30 +15,40 @@
 #log="10.0.0.3"
 #srv1="10.0.0.4"
 #srv2="10.0.0.5"
+rate1="25"
+rate2="50"
+rate3="75"
+rate4="100"
+rate5="125"
+rate6="150"
 
-echo 'log server'
+echo '***** log server'
 bash -i -l -c 'sshpass -p mininet \
 ssh -oStrictHostKeyChecking=no mininet@10.0.0.3 "sudo D-ITG/D-ITG-2.8.0-rc1/bin/ITGLog 0>/dev/null" &'
 
-echo 'client 1'
+echo '***** client 1'
 bash -i -l -c 'sshpass -p mininet \
 ssh -oStrictHostKeyChecking=no mininet@10.0.0.1 "sudo D-ITG/D-ITG-2.8.0-rc1/bin/ITGRecv 0>/dev/null" &'
-echo 'client 2'
+echo '***** client 2'
 bash -i -l -c 'sshpass -p mininet \
 ssh -oStrictHostKeyChecking=no mininet@10.0.0.2 "sudo D-ITG/D-ITG-2.8.0-rc1/bin/ITGRecv 0>/dev/null" &'
 
 echo 'UDP TESTS'
-echo 'server 1'
-bash -i -l -c 'sshpass -p mininet \
-ssh -oStrictHostKeyChecking=no mininet@10.0.0.4 "sudo ktr/ITGscripts/ITGSendUDP 10.0.0.1 1 10.0.0.3 0>/dev/null" &'
-echo 'server 2'
-bash -i -l -c 'sshpass -p mininet \
-ssh -oStrictHostKeyChecking=no mininet@10.0.0.5 "sudo ktr/ITGscripts/ITGSendUDP 10.0.0.2 2 10.0.0.3 0>/dev/null" &'
+echo '***** server 1'
+bash -i -l -c 'sshpass -p mininet ssh -oStrictHostKeyChecking=no mininet@10.0.0.4 "sudo ktr/ITGscripts/ITGSendUDP_cbr 10.0.0.1 10.0.0.3 $rate6 20000 0>/dev/null" &'
+#echo '***** server 2'
+#bash -i -l -c 'sshpass -p mininet 
+#ssh -oStrictHostKeyChecking=no mininet@10.0.0.5 "
+#sudo ktr/ITGscripts/ITGSendUDP_cbr 10.0.0.2 10.0.0.3 $rate1 10000 0>/dev/null && 
+#sudo ktr/ITGscripts/ITGSendUDP_cbr 10.0.0.2 10.0.0.3 $rate2 10000 0>/dev/null && 
+#sudo ktr/ITGscripts/ITGSendUDP_cbr 10.0.0.2 10.0.0.3 $rate3 10000 0>/dev/null && 
+#sudo ktr/ITGscripts/ITGSendUDP_cbr 10.0.0.2 10.0.0.3 $rate4 10000 0>/dev/null 
+#" &'
 
-echo 'TCP TESTS'
-echo 'server 1'
-bash -i -l -c 'sshpass -p mininet \
-ssh -oStrictHostKeyChecking=no mininet@10.0.0.4 "sudo ktr/ITGscripts/ITGSendTCP 10.0.0.1 3 10.0.0.3 0>/dev/null" &'
-echo 'server 2'
-bash -i -l -c 'sshpass -p mininet \
-ssh -oStrictHostKeyChecking=no mininet@10.0.0.5 "sudo ktr/ITGscripts/ITGSendTCP 10.0.0.2 4 10.0.0.3 0>/dev/null" &'
+#echo 'TCP TESTS'
+#echo 'server 1'
+#bash -i -l -c 'sshpass -p mininet \
+#ssh -oStrictHostKeyChecking=no mininet@10.0.0.4 "sudo ktr/ITGscripts/ITGSendTCP_cbr 10.0.0.1 10.0.0.3 tcp1_25cbr 25 0>/dev/null" &'
+#echo 'server 2'
+#bash -i -l -c 'sshpass -p mininet \
+#ssh -oStrictHostKeyChecking=no mininet@10.0.0.5 "sudo ktr/ITGscripts/ITGSendTCP_cbr 10.0.0.2 10.0.0.3 tcp2_25cbr 25 0>/dev/null" &'
