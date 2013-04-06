b348239da4f8b9cfa90bb93922ed04d9d9391976
sjas <teilzeittresor@web.de>
2013-04-06 19:55:51 +0200

    overdue backup

--- MATLAB/process_all_logs.m
+++ MATLAB/process_all_logs.m
@@ -9,7 +9,6 @@ extension_loadfile = '.dat';
 extension_imgfile  = '.png';
 prefix_imgfile     = 'pic_';
 
-
 %%% CODE
 % string concatenation and getting a file list
 wildcard        = strcat('*', extension_loadfile);
@@ -21,19 +20,93 @@ file_set        = dir(wildcard_string);
 for i = 1:length(file_set)
     current_file_name_with_ext = file_set(i).name;
     % create filename for picture by replacing the extension
-    temp_picture_file_name    = strrep(current_file_name_with_ext, extension_loadfile, extension_imgfile);
+    bare_file_name            = strrep(current_file_name_with_ext, extension_loadfile, '');
+    temp_picture_file_name    = strcat(bare_file_name, extension_imgfile);
     current_picture_file_name = strcat(prefix_imgfile, temp_picture_file_name);
 
     % load file with absolute path, 
     % since the file_set provides just the bare filename
     %%% TODO check if this can be done easier with 'file_in_loadpath(<file>)'
     current_file = loadwrapper(strcat(folder_name, current_file_name_with_ext)); 
-    parsed_data = processdata(current_file);
+    parsed_data = processdata(current_file, bare_file_name);
 
-    % save image
-    saveas(1, current_picture_file_name);
+    %%% PRODUCE DATA STRUCTURES TO BE USED FOR THE GRAPH
+    %SAVE CORRESPONDING BITRATE VALUE, THE X VALUE FOR CORRESPONDENCE
+    test_bitrate(i) = str2num(substr(current_file_name_with_ext, 25, 5));
+
+
+    %SAVE CALCULATIONS HERE FOR OVERVIEW GRAPH AT THE END
+    % mean bitrate as y value according to x
+    mean_bitrate(i,1)       = test_bitrate(i);
+    mean_bitrate(i,2)       = mean(parsed_data(i,1));
+    % standard deviation for bitrate according to each x value
+    std_dev_bitrate(i,1)    = test_bitrate(i);
+    std_dev_bitrate(i,2)    = std(parsed_data(i,1));
+    
+    % mean delay as y value according to x
+    mean_delay(i,1)         = test_bitrate(i);
+    mean_delay(i,2)         = mean(parsed_data(i,2));
+    % standard deviation for delay according to each x value
+    std_dev_delay(i,1)      = test_bitrate(i);
+    std_dev_delay(i,2)      = std(parsed_data(i,2));
+
+    % mean jitter as y value according to x
+    mean_jitter(i,1)        = test_bitrate(i);
+    mean_jitter(i,2)        = mean(parsed_data(i,3));
+    % standard deviation for jitter according to each x value
+    std_dev_jitter(i,1)     = test_bitrate(i);
+    std_dev_jitter(i,2)     = std(parsed_data(i,3));
+
+    % mean packetloss as y value according to x
+    mean_packetloss(i,1)    = test_bitrate(i);
+    mean_packetloss(i,2)    = mean(parsed_data(i,4));
+    % standard deviation for packetloss according to each x value
+    std_dev_packetloss(i,1) = test_bitrate(i);
+    std_dev_packetloss(i,2) = std(parsed_data(i,4));
+
+    % SAVE SINGLE OVERVIEW IMAGE
+    %saveas(1, current_picture_file_name);
 
 end
+
+% CREATE THE ERRORBARS FOR BETTER OVERVIEW ALTOGETHER
+%% throughput
+%subplot(4,1,1);
+figure(1);
+errorbar(test_bitrate, mean_bitrate, std_dev_bitrate);
+title('mean throughput + std. dev.');
+xlabel('bitrates');
+ylabel('bitrate value');
+line([x,x],[11800,13200]);
+%axis([0 max(t_conv) 0 round(max(bitrate_u))+1]);
+grid on
+%% delay
+%subplot(4,1,2);
+figure(2);
+errorbar(test_bitrate, mean_delay, std_dev_delay);
+title('mean delay + std. dev.');
+xlabel('bitrates');
+ylabel('delay value');
+%axis([0 max(t_conv) 0 round(max(bitrate_u))+1]);
+grid on
+%% jitter
+%subplot(4,1,3);
+figure(3);
+errorbar(test_bitrate, mean_jitter, std_dev_jitter);
+title('mean jitter + std. dev.');
+xlabel('bitrates');
+ylabel('jitter value');
+%axis([0 max(t_conv) 0 round(max(bitrate_u))+1]);
+grid on
+%% packet loss
+%subplot(4,1,4);
+figure(4);
+errorbar(test_bitrate, mean_packetloss, std_dev_packetloss);
+title('mean packetloss + std. dev.');
+xlabel('bitrates');
+ylabel('packetloss value');
+%axis([0 max(t_conv) 0 round(max(bitrate_u))+1]);
+grid on
 
 % TIDY UP
 % clear variables
@@ -44,10 +117,22 @@ clear prefix_imgfile;
 clear wildcard;
 clear wildcard_string;
 clear file_set;
+clear i;
 clear current_file_name_with_ext;
+clear bare_file_name;
 clear temp_picture_file_name;
 clear current_picture_file_name;
 clear current_file;
-clear parsed_data;
+%clear parsed_data;
+clear parse_result;
+clear test_bitrate;
+clear mean_bitrate;
+clear mean_delay;
+clear mean_jitter;
+clear mean_packetloss;
+clear std_dev_bitrate;
+clear std_dev_delay;
+clear std_dev_jitter;
+clear std_dev_packetloss;
 % close gfx window
-close all
+%close all
--- MATLAB/processdata.m
+++ MATLAB/processdata.m
@@ -1,108 +1,117 @@
-function [ x ] = processdata( matrix )
+function [ x ] = processdata( mx, matrix_name )
 
-    t_start = matrix(1,5)*3600+matrix(1,6)*60+matrix(1,7);
-    t_conv = (matrix(:,5)*3600+matrix(:,6)*60+matrix(:,7)) - t_start;
-    t_start_s = matrix(1,2)*3600+matrix(1,3)*60+matrix(1,4);
-    t_conv_s = (matrix(:,2)*3600+matrix(:,3)*60+matrix(:,4)) - t_start_s;
+    t_start   = mx(1,5)  * 3600 + mx(1,6) * 60 + mx(1,7);
+    t_conv    = (mx(:,5) * 3600 + mx(:,6) * 60 + mx(:,7)) - t_start;
+    t_start_s = mx(1,2)  * 3600 + mx(1,3) * 60 + mx(1,4);
+    t_conv_s  = (mx(:,2) * 3600 + mx(:,3) * 60 + mx(:,4)) - t_start_s;
 
     % compute intervals
     j = 1;
     t_int = 0;
-    rate(j) = 0;
+
+    bitrate(j) = 0;
     delay(j) = 0;
     jitter(j) = 0;
     pktloss(j) = 0;
 
-    for i = 1:length(matrix)
+    for i = 1:length(mx)
+
+        % EITHER
         if (t_conv(i) - t_int >= 1)
-            j=j+1;
-            t_int = t_conv(i);
-            rate(j) = matrix(i,8);
-            delay(j) = t_conv(i) - t_conv_s(i);
-            if (i > 1)
-                pktloss(j) = matrix(i,1) - matrix(i-1,1) - 1;
-                jitter(j) = t_conv(i) - t_conv(i-1);
-            end
-        else
-            rate(j) = rate(j) + matrix(i,8);
-            delay(j) = mean([delay(j) (t_conv(i) - t_conv_s(i))]);
+            j          = j + 1;
+            t_int      = t_conv(i);
+            bitrate(j) = mx(i,8);
+            delay(j)   = t_conv(i) - t_conv_s(i);
 
             if (i > 1)
-                pktloss(j) = pktloss(j) + matrix(i,1) - matrix(i-1,1) - 1;
-                jitter(j) = mean([jitter(j) (t_conv(i) - t_conv(i-1))]);
+                pktloss(j) = mx(i,1) - mx(i-1,1) - 1;
+                jitter(j)  = t_conv(i) - t_conv(i-1);
+            end
+
+        % OR
+        else
+
+            bitrate(j) = bitrate(j) + mx(i,8);
+            delay(j)   = mean([delay(j) (t_conv(i) - t_conv_s(i))]);
+
+            if (i > 1)
+                pktloss(j) = pktloss(j) + mx(i,1) - mx(i-1,1) - 1;
+                jitter(j)  = mean([jitter(j) (t_conv(i) - t_conv(i-1))]);
             end
         end
     end
 
-    x=[rate' delay' jitter' pktloss'];
+    %x = [bitrate'];
+    x = [bitrate' delay' jitter' pktloss'];
 
-    % Throughput
-    subplot (4, 2, 1);
-    rate_u = rate ./ 125;
-    plot(0:j-2, rate_u(1:j-1),'-');
-    title('Throughput');
-    xlabel('time [s]');
-    ylabel('[Kbps]');
-    axis([0 max(t_conv) 0 round(max(rate_u))+1]);
-    grid on
-    %hold on;
+    %% Throughput
+    %subplot (4, 2, 1);
+    %bitrate_u = bitrate ./ 125;
+    %plot(0:j-2, bitrate_u(1:j-1),'-');
+    %title('Throughput');
+    %xlabel('time [s]');
+    %ylabel('[Kbps]');
+    %axis([0 max(t_conv) 0 round(max(bitrate_u))+1]);
+    %grid on
+    %%hold on;
 
-    % Delay
-    subplot (4, 2, 2);
-    plot(0:length(delay)-1, delay,'-');
-    title('Delay');
-    xlabel('time [s]');
-    ylabel('[ms]');
-    axis([0 max(t_conv) min(delay)-0.00001 max(delay)]);
-    grid on
-    %hold on;
+    %% Delay
+    %subplot (4, 2, 2);
+    %plot(0:length(delay)-1, delay,'-');
+    %title('Delay');
+    %xlabel('time [s]');
+    %ylabel('[ms]');
+    %axis([0 max(t_conv) min(delay)-0.00001 max(delay)]);
+    %grid on
+    %%hold on;
 
-    % Jitter
-    subplot (4, 2, 3);
-    plot(0:length(jitter)-1, jitter,'-');
-    title('Jitter');
-    xlabel('time [s]');
-    ylabel('[ms]');
-    axis([0 max(t_conv) min(jitter)-0.00001 max(jitter)+0.00001]);
-    grid on
+    %% Jitter
+    %subplot (4, 2, 3);
+    %plot(0:length(jitter)-1, jitter,'-');
+    %title('Jitter');
+    %xlabel('time [s]');
+    %ylabel('[ms]');
+    %axis([0 max(t_conv) min(jitter)-0.00001 max(jitter)+0.00001]);
+    %grid on
 
-    % Packet loss
-    subplot (4, 2, 4);
-    plot(0:length(pktloss)-1, pktloss,'-');
-    title('Packet loss');
-    xlabel('time [s]');
-    ylabel('[pps]');
-    axis([0 max(t_conv) 0 round(max(pktloss))+1]);
-    grid on
+    %% Packet loss
+    %subplot (4, 2, 4);
+    %plot(0:length(pktloss)-1, pktloss,'-');
+    %title('Packet loss');
+    %xlabel('time [s]');
+    %ylabel('[pps]');
+    %axis([0 max(t_conv) 0 round(max(pktloss))+1]);
+    %grid on
 
-    % IDT distribution
-    subplot (4, 2, 5);
-    d = diff(t_conv);
-    m = max(d);
-    hist(d);
-    title('Inter-departure time Distribution');
-    xlabel('time [s]');
-    ylabel('Empirical PDF');
-    %axis([0 max([1 m]) 0 1]);
-    grid on
+    %% IDT distribution
+    %subplot (4, 2, 5);
+    %d = diff(t_conv);
+    %m = max(d);
+    %hist(d);
+    %title('Inter-departure time Distribution');
+    %xlabel('time [s]');
+    %ylabel('Empirical PDF');
+    %%axis([0 max([1 m]) 0 1]);
+    %grid on
 
-    % PS distribution
-    subplot (4, 2, 6);
-    hist(matrix(:,8));
-    title('Packet size Distribution');
-    xlabel('[bytes]');
-    ylabel('Empirical PDF');
-    grid on
+    %% PS distribution
+    %subplot (4, 2, 6);
+    %hist(mx(:,8));
+    %title('Packet size Distribution');
+    %xlabel('[bytes]');
+    %ylabel('Empirical PDF');
+    %grid on
 
-    %errorbar
-    subplot (4, 2, 7);
-    %errorbar();
-    title('errorbar');
-    xlabel('xlabel');
-    ylabel('ylabel');
-    grid on
+    %%errorbar
+    %subplot (4, 2, 7);
+    %%errorbar();
+    %title('errorbar');
+    %xlabel('xlabel');
+    %ylabel('ylabel');
+    %grid on
 
-    %dummy
-    subplot (4, 2, 8);
+    %%dummy
+    %subplot (4, 2, 8);
+    %xlabel(matrix_name);
 
 end
