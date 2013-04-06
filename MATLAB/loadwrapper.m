56e59ad23362b859c4b1baa78fcbc4dde5ff64cd
sjas <teilzeittresor@web.de>
2013-04-06 05:16:13 +0200

    minor

--- MATLAB/loadwrapper.m
+++ MATLAB/loadwrapper.m
@@ -5,7 +5,7 @@ function [ loaded_data ] = loadwrapper( file_name )
 
     files = dir(file_name);
      for k = 1:length(files)
-         loaded_data = load(files(k).name);
+         loaded_data = load(files(k).name, '-ascii');
      end
 
  end
