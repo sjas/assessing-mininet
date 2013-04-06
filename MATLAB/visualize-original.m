508e347b7be9730a0a0ce756fc47d09e006fa132
sjas <teilzeittresor@web.de>
2013-04-06 04:58:18 +0200

    added initial version of visualize script

--- MATLAB/visualize-original.m
+++ MATLAB/visualize-original.m
@@ -39,26 +39,24 @@ function [ x ] = v( test )
     x=[rate' delay' jitter' pktloss'];
  
 	% Throughput
-	subplot (4, 2, 1);
+	subplot (3, 2, 1);
 	rate_u = rate ./ 125;
 	plot(0:j-2, rate_u(1:j-1),'-');
 	title('Throughput');
 	xlabel('time [s]');
 	ylabel('[Kbps]');
 	axis([0 max(t_conv) 0 round(max(rate_u))+1]);
-    %hold on;
-
+	
 	% Delay
-	subplot (4, 2, 2);
+	subplot (3, 2, 2);
 	plot(0:length(delay)-1, delay,'-');
 	title('Delay');
 	xlabel('time [s]');
 	ylabel('[ms]');
 	axis([0 max(t_conv) min(delay)-0.00001 max(delay)]);
-    %hold on;
     
 	% Jitter
-	subplot (4, 2, 3);
+	subplot (3, 2, 3);
 	plot(0:length(jitter)-1, jitter,'-');
 	title('Jitter');
 	xlabel('time [s]');
@@ -66,7 +64,7 @@ function [ x ] = v( test )
 	axis([0 max(t_conv) min(jitter)-0.00001 max(jitter)+0.00001]);
     
 	% Packet loss
-	subplot (4, 2, 4);
+	subplot (3, 2, 4);
 	plot(0:length(pktloss)-1, pktloss,'-');
 	title('Packet loss');
 	xlabel('time [s]');
@@ -74,7 +72,7 @@ function [ x ] = v( test )
 	axis([0 max(t_conv) 0 round(max(pktloss))+1]);
     
 	% IDT distribution
-	subplot (4, 2, 5);
+	subplot (3, 2, 5);
 	d = diff(t_conv);
 	m = max(d);
 	hist(d);
@@ -84,21 +82,12 @@ function [ x ] = v( test )
 	%axis([0 max([1 m]) 0 1]);
     
 	% PS distribution
-	subplot (4, 2, 6);
+	subplot (3, 2, 6);
 	hist(test(:,8));
 	title('Packet size Distribution');
 	xlabel('[bytes]');
 	ylabel('Empirical PDF');
     
-    %errorbar
-    subplot (4, 2, 7);
-    %errorbar();
-	title('errorbar');
-	xlabel('xlabel');
-	ylabel('ylabel');
-    
-    %dummy
-    subplot (4, 2, 8);
     
 end
 
