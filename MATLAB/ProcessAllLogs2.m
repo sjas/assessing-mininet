1d4a3ffd2bd53d66cdf08788fd6c28f83c88a5f6
sjas <teilzeittresor@web.de>
2013-04-08 14:57:56 +0200

    fix? hope so

--- MATLAB/ProcessAllLogs2.m
+++ MATLAB/ProcessAllLogs2.m
@@ -1,7 +1,7 @@
 
 %%% convenience function to generate all plots at once
 
-process_all_logs(tcp_snd);
-process_all_logs(tcp_rcv);
-process_all_logs(udp_snd);
-process_all_logs(udp_rcv);
+process_all_logs('tcp_snd');
+process_all_logs('tcp_rcv');
+process_all_logs('udp_snd');
+process_all_logs('udp_rcv');
--- MATLAB/process_all_logs.m
+++ MATLAB/process_all_logs.m
@@ -96,8 +96,8 @@ function [ ] = process_all_logs( log_type )
     s_mean_bitrate    = min(mean_bitrate)    * 0.9;
     s_mean_jitter     = min(mean_jitter)     * 0.9;
     e_mean_jitter     = max(mean_jitter)     * 1.1;
-    s_mean_delay      = min(mean_delay)      * 0.9;
-    e_mean_delay      = max(mean_delay)      * 1.1;
+    s_mean_delay      = min(mean_delay)      ;
+    e_mean_delay      = max(mean_delay)      ;
     s_mean_packetloss = min(mean_packetloss) * 0.9-0.1;
     e_mean_packetloss = max(mean_packetloss) * 1.1+0.1;
 
