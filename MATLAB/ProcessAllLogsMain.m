23eefc44e8a6e1c1ad36ca62d140c418df964100
sjas <teilzeittresor@web.de>
2013-07-15 16:33:40 +0200

    cleanup and committing current matlab code

---
+++ MATLAB/ProcessAllLogsMain.m
@@ -0,0 +1,93 @@
+%% convenience function to parse all data collections at once
+%% if crosstraffic is tested, other axes are needed for graphs, thus the switch
+
+%  There are many magic numbers in here, cannot help that until there is no
+%  system redesign coming, during when i.e. config files are transmitted, the
+%  transmitted test data will be placed in special folders, etc. etc. etc.
+%  Without a system redesign up front changing how test files are
+%  generated, not much can be done here. 
+%  Try and fail, every other solution will also just be like 'walking with crutches'.
+
+%% enable processing of each of the available test sets
+% setup / initialize
+process_test_set_01 = 0;
+process_test_set_02 = 0;
+process_test_set_03 = 0;
+process_test_set_04 = 0;
+process_test_set_05 = 0;
+process_test_set_06 = 0;
+crosstraffic_switch_on = 1;
+crosstraffic_switch_off = 0;
+%% ============================================================================================
+%% ============================ comment out if unneeded =======================================
+%% ============================================================================================
+                                %  1 is on, 0 is off
+%                                 process_test_set_01 = 1;
+%                                 process_test_set_02 = 1;
+%                                 process_test_set_03 = 1;
+%                                 process_test_set_04 = 1;
+                                process_test_set_05 = 1;
+                                process_test_set_06 = 1;
+%% ============================================================================================
+%% ============================================================================================
+%% ============================================================================================
+
+%% folders containing test data
+folder_01 = '/home/ktr/Documents/MATLAB/Mininet/logs1/';
+folder_02 = '/home/ktr/Documents/MATLAB/Mininet/logs2/';
+folder_03 = '/home/ktr/Documents/MATLAB/Mininet/logs3/';
+folder_04 = '/home/ktr/Documents/MATLAB/Mininet/logs4/';
+folder_05 = '/home/ktr/Documents/MATLAB/Mininet/logs5/';
+folder_06 = '/home/ktr/Documents/MATLAB/Mininet/logs6/';
+%% testsuite id
+test_suite_prefix_01 = 'testsuite_01_lineartopo-lonetraffic';
+% test_suite_prefix_02 = 'testsuite_02_lineartopo-crosstraffic';
+% test_suite_prefix_03 = 'testsuite_03_meshedtopo-lonetraffic';
+% test_suite_prefix_04 = 'testsuite_04_meshedtopo-crosstraffic';
+% test_suite_prefix_05 = 'testsuite_05_lineartopo-crosstraffic-2x10000';
+% test_suite_prefix_06 = 'testsuite_06_meshedtopo-crosstraffic-2x10000';
+
+
+%% no need to change anything below this line
+% non-meshed topology, single traffic source
+if (process_test_set_01 == 1)
+    process_complete_test_set('tcp_rcv', test_suite_prefix_01 , folder_01, crosstraffic_switch_off);
+    process_complete_test_set('tcp_snd', test_suite_prefix_01 , folder_01, crosstraffic_switch_off);
+%     process_complete_test_set('udp_rcv', test_suite_prefix_01 , folder_01, crosstraffic_switch_off);
+%     process_complete_test_set('udp_snd', test_suite_prefix_01 , folder_01, crosstraffic_switch_off);
+end
+% non-meshed topology, crosstraffic with two sources
+if (process_test_set_02 == 1)
+    process_complete_test_set('tcp_rcv', test_suite_prefix_02 , folder_02, crosstraffic_switch_on);
+    process_complete_test_set('tcp_snd', test_suite_prefix_02 , folder_02, crosstraffic_switch_on);
+    process_complete_test_set('udp_rcv', test_suite_prefix_02 , folder_02, crosstraffic_switch_on);
+    process_complete_test_set('udp_snd', test_suite_prefix_02 , folder_02, crosstraffic_switch_on);
+end
+% meshed topology, single traffic source
+if (process_test_set_03 == 1)
+    process_complete_test_set('tcp_rcv', test_suite_prefix_03 , folder_03, crosstraffic_switch_off);
+    process_complete_test_set('tcp_snd', test_suite_prefix_03 , folder_03, crosstraffic_switch_off);
+    process_complete_test_set('udp_rcv', test_suite_prefix_03 , folder_03, crosstraffic_switch_off);
+    process_complete_test_set('udp_snd', test_suite_prefix_03 , folder_03, crosstraffic_switch_off);
+end
+% meshed topology, crosstraffic with two sources
+if (process_test_set_04 == 1)
+    process_complete_test_set('tcp_rcv', test_suite_prefix_04 , folder_04, crosstraffic_switch_on);
+    process_complete_test_set('tcp_snd', test_suite_prefix_04 , folder_04, crosstraffic_switch_on);
+    process_complete_test_set('udp_rcv', test_suite_prefix_04 , folder_04, crosstraffic_switch_on);
+    process_complete_test_set('udp_snd', test_suite_prefix_04 , folder_04, crosstraffic_switch_on);
+end
+% non-meshed topology, crosstraffic with two sources
+if (process_test_set_05 == 1)
+    process_complete_test_set('tcp_rcv', test_suite_prefix_05 , folder_05, crosstraffic_switch_off);
+    process_complete_test_set('tcp_snd', test_suite_prefix_05 , folder_05, crosstraffic_switch_off);
+    process_complete_test_set('udp_rcv', test_suite_prefix_05 , folder_05, crosstraffic_switch_off);
+    process_complete_test_set('udp_snd', test_suite_prefix_05 , folder_05, crosstraffic_switch_off);
+end
+% meshed topology, crosstraffic with two sources
+if (process_test_set_06 == 1)
+%     process_complete_test_set('tcp_rcv', test_suite_prefix_06 , folder_06, crosstraffic_switch_off);
+%     process_complete_test_set('tcp_snd', test_suite_prefix_06 , folder_06, crosstraffic_switch_off);
+    process_complete_test_set('udp_rcv', test_suite_prefix_06 , folder_06, crosstraffic_switch_off);
+    process_complete_test_set('udp_snd', test_suite_prefix_06 , folder_06, crosstraffic_switch_off);
+end
---
+++ MATLAB/_deprecated/process_data.m
@@ -0,0 +1,107 @@
+function [ x ] = process_data( mx, matrix_name )
+
+    t_start   = mx(1,5)  * 3600 + mx(1,6) * 60 + mx(1,7);
+    t_conv    = (mx(:,5) * 3600 + mx(:,6) * 60 + mx(:,7)) - t_start;
+    t_start_s = mx(1,2)  * 3600 + mx(1,3) * 60 + mx(1,4);
+    t_conv_s  = (mx(:,2) * 3600 + mx(:,3) * 60 + mx(:,4)) - t_start_s;
+
+    % compute intervals
+    jj     = 1;
+    t_int = 0;
+
+    bitrate(jj) = 0;
+    delay(jj)   = 0;
+    jitter(jj)  = 0;
+    pktloss(jj) = 0;
+
+
+    for ii = 1:length(mx)
+
+        % EITHER
+        if (t_conv(ii) - t_int >= 1)
+            jj          = jj + 1;
+            t_int       = t_conv(ii);
+            bitrate(jj) = mx(ii,8);
+            delay(jj)   = t_conv(ii) - t_conv_s(ii);
+
+            if (ii > 1)
+                pktloss(jj) = mx(ii,1) - mx(ii-1,1) - 1;
+                jitter(jj)  = t_conv(ii) - t_conv(ii-1);
+            end
+
+        % OR
+        else
+
+            bitrate(jj) = bitrate(jj) + mx(ii,8);
+            delay(jj)   = mean([delay(jj) (t_conv(ii) - t_conv_s(ii))]);
+
+            if (ii > 1)
+                pktloss(jj) = pktloss(jj) + mx(ii,1) - mx(ii-1,1) - 1;
+                jitter(jj)  = mean([jitter(jj) (t_conv(ii) - t_conv(ii-1))]);
+            end
+        end
+    end
+
+    %x = [bitrate'];
+    x = [bitrate' delay' jitter' pktloss'];
+
+    % Throughput
+    subplot (3, 2, 1);
+    bitrate_u = bitrate ./ 125;
+    plot(0:j-2, bitrate_u(1:j-1),'-');
+    title('Throughput');
+    xlabel('time [s]');
+    ylabel('[Kbps]');
+    axis([0 max(t_conv) 0 round(max(bitrate_u))+1]);
+    grid on
+    %hold on;
+
+    % Delay
+    subplot (3, 2, 2);
+    plot(0:length(delay)-1, delay,'-');
+    title('Delay');
+    xlabel('time [s]');
+    ylabel('[ms]');
+    axis([0 max(t_conv) min(delay)-0.00001 max(delay)]);
+    grid on
+    %hold on;
+
+    % Jitter
+    subplot (3, 2, 3);
+    plot(0:length(jitter)-1, jitter,'-');
+    title('Jitter');
+    xlabel('time [s]');
+    ylabel('[ms]');
+    axis([0 max(t_conv) min(jitter)-0.00001 max(jitter)+0.00001]);
+    grid on
+
+    % Packet loss
+    subplot (3, 2, 4);
+    plot(0:length(pktloss)-1, pktloss,'-');
+    title('Packet loss');
+    xlabel('time [s]');
+    ylabel('[pps]');
+    axis([0 max(t_conv) 0 round(max(pktloss))+1]);
+    grid on
+
+    % IDT distribution
+    subplot (3, 2, 5);
+    d = diff(t_conv);
+    m = max(d);
+    hist(d);
+    title('Inter-departure time Distribution');
+    xlabel('time [s]');
+    ylabel('Empirical PDF');
+    %axis([0 max([1 m]) 0 1]);
+    grid on
+
+    % PS distribution
+    subplot (3, 2, 6);
+    hist(mx(:,8));
+    title('Packet size Distribution');
+    xlabel('[bytes]');
+    ylabel('Empirical PDF');
+    grid on
+
+
+end
