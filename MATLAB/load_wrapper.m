3900aa28bd16de8a67657887553db8db6b765e64
sjas <teilzeittresor@web.de>
2013-07-15 16:34:49 +0200

    cleanup

--- MATLAB/load_wrapper.m
+++
@@ -1,9 +0,0 @@
-function [ loaded_data ] = load_wrapper( file_name )
-%%% will load a file if its filename is provided
-%%% USAGE: (save data to var)
-%%% x =loadwrapper('<filenam>')
-
-    files = dir(file_name);
-    loaded_data = load(files.name, '-ascii');
-
-end
